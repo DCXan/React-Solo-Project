19d7f27c0e56bba538c3264e05d81e7232ded0df
DCXan <chen.david.1016@gmail.com>
2022-07-28 15:31:09 -0500

    enabled users to select a specific room to chat in

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,56 @@
 import './App.css';
 import React from 'react'
 import io from 'socket.io-client'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const socket = io.connect('http://localhost:3001')
 
 function App() {
 
-  const sendMessage = () => {
+  // Room State
+  const [room, setRoom] = useState('')
 
-    socket.emit("send_message", { message: 'hello'})
+  // Messages State
+  const [message, setMessage] = useState('')
+  const [messageDisplay, setMessageDisplay] = useState('')
+
+  const roomInput = (e) => {
+    setRoom(e.target.value)
+  }
+
+  const joinRoom = () => {
+    if (room !== '') {
+      socket.emit("join_room", room)
+    }
+  }
+
+  const messageInput = (e) => {
+    setMessage(e.target.value)
+  }
+
+  const sendMessage = () => {
+    socket.emit("send_message", {message, room})
   }
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
-      alert(data.message)
+      setMessageDisplay(data.message)
     })
   }, [socket])
 
   return (
     <div className="App">
-      <input placeholder="Message"/>
-      <button onClick={sendMessage}>Send Message</button>
+      <div>
+        <input onChange={roomInput} placeholder="Select Room" name='room'/>
+        <button onClick={joinRoom}>Join Room</button>
+      </div>
+      <div>
+        <input onChange={messageInput} placeholder="Message" name='message'/>
+        <button onClick={sendMessage}>Send Message</button>
+      </div>
+      <h1>Message History</h1>
+      <h4>{messageDisplay}</h4>
+    
     </div>
   );
 }
